1ebf6d4cf57f3b4dd07c7c127a864e695acf80a2
cesarous <cero@umich.edu>
2024-03-27 00:13:09 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,26 +4,19 @@ import { Box, Flex, Link, Spacer } from '@chakra-ui/react';
 
 function Navbar() {
   return (
-    <Box bg="#283d90" py="3">
-      <Flex maxW="1200px" mx="auto" px="4">
-        <Link href="#home" fontSize="xl" fontWeight="bold" color="white">Home</Link>
-        <Spacer id="home" />
-        <Box sx={{marginLeft:"10%"}}>
-          <Box>
-            <Link href="#aboutus-section"  mr="4" color="white">About Us</Link>
-          </Box>
-          <Box>
-            <Link href="#services-list" mr="4" color="white">Services</Link>
-          </Box>
-          <Box>
-            <Link href="#map" mr="4" color="white">Map</Link>
-          </Box>
-          <Box>
-            <Link href="#contact-form" mr="4" color="white">Contact Us</Link>
-          </Box>
-        </Box>
-      </Flex>
+<Box bg="#283d90" py="3">
+  <Flex maxW="1200px" mx="auto" px="4" justifyContent="space-between" alignItems="center">
+    <Link href="#home" fontSize="xl" fontWeight="bold" color="white">Home</Link>
+    <Spacer />
+    <Box sx={{ display: { base: "none", md: "block" } }}>
+      <Link href="#aboutus-section" mr="4" color="white">About Us</Link>
+      <Link href="#services-list" mr="4" color="white">Services</Link>
+      <Link href="#map" mr="4" color="white">Map</Link>
+      <Link href="#contact-form" color="white">Contact Us</Link>
     </Box>
+  </Flex>
+</Box>
+
   );
 }
 
